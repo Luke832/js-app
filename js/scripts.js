e1b74e66fdcee1d02647ec466bc44d21f9c79262
Luke832 <72308118+Luke832@users.noreply.github.com>
2020-11-01 10:48:16 -0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+alert('Hello World');
+let myName = 'Luke';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+let simpleAddition = 2 +2;
+document.write(simpleAddition);
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+let favoriteFood = 'Pizza';
+document.write(favoriteFood);
