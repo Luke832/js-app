1a53d035fffd488a18a09bb557bebef45412c9ae
Luke832 <72308118+Luke832@users.noreply.github.com>
2020-11-05 11:05:09 -0500

    Updated Syntax
    changed the semicolons to commas after each key value pair and added commas after each object

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,32 @@
 let pokemonList = [
   {
-    name: 'Bulbasaur';
-    height: 0.7;
-    type: ['grass' , 'poison'];
-  }
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['grass' , 'poison'],
+  },
   {
-    name: 'Charmeleon';
-    height: 1.1;
-    type: ['fire'];
-  }
+    name: 'Charmeleon',
+    height: 1.1,
+    type: ['fire'],
+  },
   {
-    name: 'Blastoise';
-    height: 1.6;
-    type: ['water'];
-  }
+    name: 'Blastoise',
+    height: 1.6,
+    type: ['water'],
+  },
   {
-    name: 'Caterpie';
-    height: 0.3;
-    type: ['bug'];
-  }
+    name: 'Caterpie',
+    height: 0.3,
+    type: ['bug'],
+  },
   {
-    name: 'Weedle';
-    height: 0.3;
-    type: ['bug' , 'poison'];
-  }
+    name: 'Weedle',
+    height: 0.3,
+    type: ['bug' , 'poison'],
+  },
   {
-    name: 'Fearow';
-    height: 1.2;
-    type: ['flying' , 'normal'];
+    name: 'Fearow',
+    height: 1.2,
+    type: ['flying' , 'normal'],
   }
 ]
